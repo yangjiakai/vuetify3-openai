b9d2484a05fac8c9ba10247a4794b24497ef5232
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-02-25 23:17:07 +0900

    Update openAIApi.ts

--- src/api/openAIApi.ts
+++ src/api/openAIApi.ts
@@ -14,7 +14,7 @@ export const createCompletion: any = (keyword: string) => {
     // prompt: generatePrompt(keyword),
     prompt: keyword,
     temperature: 0.6,
-    max_tokens: 100,
+    max_tokens: 400,
   });
 };
 
