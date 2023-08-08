e983946f55c5fb8c2fcfea9d1d0302eb4953ab6f
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-08-09 00:25:10 +0900

    文本翻译功能和收藏功能完成

--- src/views/spoken/SpokenMessageCard.vue
+++ src/views/spoken/SpokenMessageCard.vue
@@ -5,11 +5,13 @@
 -->
 <script setup lang="ts">
 import { useSpeechStore } from "@/stores/speechStore";
+import { useSnackbarStore } from "@/stores/snackbarStore";
 import { type Message } from "@/types/spokenTypes";
 import { MdPreview } from "md-editor-v3";
 import "md-editor-v3/lib/style.css";
 import { formatForTTS } from "@/utils/aiUtils";
 import { Icon } from "@iconify/vue";
+import { readStream } from "@/utils/aiUtils";
 const speechStore = useSpeechStore();
 interface Props {
   message: Message;
@@ -17,8 +19,11 @@ interface Props {
   role?: "user" | "assistant" | "system";
   dateTime?: string;
 }
-
+const snackbarStore = useSnackbarStore();
 const props = defineProps<Props>();
+const content = computed(() => {
+  return props.message.body.content;
+});
 
 const readMessage = () => {
   const config = {
@@ -29,9 +34,69 @@ const readMessage = () => {
     VoiceName: "zh-CN-XiaoxiaoNeural",
   };
 
-  const text = formatForTTS(props.message.body.content);
+  const text = formatForTTS(content.value);
 
   speechStore.ssmlToSpeech(text, config);
+};
+
+const translatedContent = ref("");
+const isTanslating = ref(false);
+const isCollected = ref(false);
+
+const translation = async () => {
+  isTanslating.value = true;
+  try {
+    const completion = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          messages: [
+            { role: "system", content: "你是一名翻译官,将内容翻译成英文" },
+            { role: "user", content: content.value },
+          ],
+          model: "gpt-3.5-turbo",
+          stream: true,
+        }),
+      }
+    );
+
+    // Handle errors
+    if (!completion.ok) {
+      const errorData = await completion.json();
+      snackbarStore.showErrorMessage(errorData.error.message);
+      return;
+    }
+
+    // Create a reader
+    const reader = completion.body?.getReader();
+    if (!reader) {
+      snackbarStore.showErrorMessage("Cannot read the stream.");
+    }
+
+    translatedContent.value = "";
+    // Read the stream
+    readStream(reader, (message) => {
+      translatedContent.value = translatedContent.value += message;
+    });
+  } catch (error) {
+    snackbarStore.showErrorMessage(error.message);
+  } finally {
+    isTanslating.value = false;
+  }
+};
+
+const handleCollect = () => {
+  isCollected.value = !isCollected.value;
+  if (!isCollected.value) {
+    snackbarStore.showSuccessMessage("已取消收藏");
+  } else {
+    snackbarStore.showSuccessMessage("已收藏");
+  }
 };
 </script>
 
@@ -46,6 +111,12 @@ const readMessage = () => {
         <v-card class="rounded-xl rounded-bs-0">
           <div>
             <MdPreview :modelValue="props.message.body.content" />
+            <div
+              v-if="translatedContent"
+              class="mx-5 mb-5 text-body-2 text-blue-grey-darken-2"
+            >
+              {{ translatedContent }}
+            </div>
             <div class="toolbox px-5 pb-5">
               <v-btn
                 class="mr-3"
@@ -76,16 +147,31 @@ const readMessage = () => {
                 class="mr-3"
                 color="grey-lighten-1"
                 variant="text"
-                @click=""
+                @click="translation"
                 size="20"
               >
                 <template v-slot:prepend>
-                  <v-icon class="tool-icon" size="18">mdi-translate</v-icon>
+                  <v-icon
+                    :color="isTanslating ? '#705CF6' : ''"
+                    class="tool-icon"
+                    size="18"
+                    >mdi-translate</v-icon
+                  >
                 </template>
               </v-btn>
-              <v-btn variant="text" @click="" size="20" color="grey-lighten-1">
+              <v-btn
+                variant="text"
+                @click="handleCollect"
+                size="20"
+                color="grey-lighten-1"
+              >
                 <template v-slot:prepend>
-                  <v-icon class="tool-icon" size="18">mdi-heart-outline</v-icon>
+                  <v-icon
+                    :color="isCollected ? '#705CF6' : ''"
+                    class="tool-icon"
+                    size="18"
+                    >mdi-heart-outline</v-icon
+                  >
                 </template>
               </v-btn>
             </div>
