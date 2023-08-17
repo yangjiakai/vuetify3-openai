5c79acc26df5ecf06eb5685db75dd1b8d6b27e4e
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-08-17 23:09:53 +0900

    Update ConfigDialog.vue

--- src/components/ConfigDialog.vue
+++ src/components/ConfigDialog.vue
@@ -28,39 +28,101 @@ watch(
     }
   }
 );
+
+const handleCancel = () => {
+  dialog.value = false;
+};
+
+const handleSave = () => {
+  //   appStore.setConfig(config.value);
+  dialog.value = false;
+};
 </script>
 
 <template>
   <v-dialog v-model="appStore.configDialog" width="600">
     <v-card>
-      <v-card-title> 请输入您的API KEY </v-card-title>
       <hr />
-      <v-card-text>
-        <!-- ---------------------------------------------- -->
-        <!-- Model -->
-        <!-- ---------------------------------------------- -->
-        <v-label class="font-weight-medium mb-2 ml-2 mt-5"> 模型 </v-label>
-        <div style="width: 220px" class="flex ml-2 bg-grey-lighten-3 pa-2">
-          <v-btn
-            :variant="config?.model === 'gpt-3.5-turbo' ? 'flat' : 'text'"
-            color="primary"
-            class="flex-fill mr-3"
-            @click="config.model = 'gpt-3.5-turbo'"
-            >GPT-3.5</v-btn
-          >
-          <v-btn
-            :variant="config?.model === 'gpt-4' ? 'flat' : 'text'"
-            color="primary"
-            class="flex-fill"
-            @click="config.model = 'gpt-4'"
-            >GPT-4.0</v-btn
-          >
-        </div>
-      </v-card-text>
 
+      <v-card-title class="font-weight-bold text-grey-darken-2">
+        当前对话设定
+      </v-card-title>
+
+      <v-divider></v-divider>
+      <v-card-text>
+        <!-- Rate -->
+        <v-row class="align-center">
+          <v-col cols="12" sm="2" class="pb-sm-3 pb-0 text-right">
+            <v-label class="font-weight-bold text-grey-darken-2"
+              >语言模型</v-label
+            >
+          </v-col>
+          <v-col cols="12" sm="10">
+            <div
+              style="width: 220px"
+              class="flex ml-2 bg-grey-lighten-3 pa-2 rounded"
+            >
+              <v-btn
+                :variant="config?.model === 'gpt-3.5-turbo' ? 'flat' : 'text'"
+                color="primary"
+                class="flex-fill mr-3"
+                @click="config.model = 'gpt-3.5-turbo'"
+                >GPT-3.5</v-btn
+              >
+              <v-btn
+                :variant="config?.model === 'gpt-4' ? 'flat' : 'text'"
+                color="primary"
+                class="flex-fill"
+                @click="config.model = 'gpt-4'"
+                >GPT-4.0</v-btn
+              >
+            </div>
+          </v-col>
+        </v-row>
+        <!-- Rate -->
+        <v-row class="align-center">
+          <v-col cols="12" sm="2" class="pb-sm-3 pb-0 text-right">
+            <v-label class="font-weight-bold text-grey-darken-2"
+              >自由度</v-label
+            >
+          </v-col>
+          <v-col cols="12" sm="10">
+            <v-slider
+              color="primary"
+              value="1.0"
+              thumb-label="always"
+              :min="0.1"
+              :max="2"
+              :step="0.1"
+              hide-details
+            ></v-slider>
+          </v-col>
+        </v-row>
+
+        <!-- Emotion -->
+        <v-row class="align-center mb-3">
+          <v-col cols="12" sm="2" class="pb-sm-3 pb-0 text-right">
+            <v-label class="font-weight-bold text-grey-darken-2">风格</v-label>
+          </v-col>
+          <v-col cols="12" sm="10">
+            <v-select
+              color="primary"
+              variant="outlined"
+              hide-details
+              density="compact"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-divider></v-divider>
       <v-card-actions>
+        <v-btn color="grey" class="font-weight-bold" @click="handleCancel"
+          >取消修改</v-btn
+        >
         <v-spacer></v-spacer>
-        <v-btn block color="primary" @click="dialog = false">关闭</v-btn>
+        <v-btn color="primary" class="font-weight-bold" @click="handleSave"
+          >保存配置</v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-dialog>
