f2a7cc07ac0b75fa9b9dea82deb95e4ffcb4a244
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-09-03 02:33:00 +0900

    语言模型数据获取时机优化

---
+++ src/data/characterData.ts
@@ -0,0 +1,728 @@
+export default [
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Jenny Multilingual",
+        "fullName": "English (United States)",
+        "modelName": "en-US-JennyMultilingualNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Jenny",
+        "fullName": "English (United States)",
+        "modelName": "en-US-JennyNeural",
+        "styleList": [
+            "assistant",
+            "chat",
+            "customerservice",
+            "newscast",
+            "angry",
+            "cheerful",
+            "sad",
+            "excited",
+            "friendly",
+            "terrified",
+            "shouting",
+            "unfriendly",
+            "whispering",
+            "hopeful"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Guy",
+        "fullName": "English (United States)",
+        "modelName": "en-US-GuyNeural",
+        "styleList": [
+            "newscast",
+            "angry",
+            "cheerful",
+            "sad",
+            "excited",
+            "friendly",
+            "terrified",
+            "shouting",
+            "unfriendly",
+            "whispering",
+            "hopeful"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Aria",
+        "fullName": "English (United States)",
+        "modelName": "en-US-AriaNeural",
+        "styleList": [
+            "chat",
+            "customerservice",
+            "narration-professional",
+            "newscast-casual",
+            "newscast-formal",
+            "cheerful",
+            "empathetic",
+            "angry",
+            "sad",
+            "excited",
+            "friendly",
+            "terrified",
+            "shouting",
+            "unfriendly",
+            "whispering",
+            "hopeful"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Davis",
+        "fullName": "English (United States)",
+        "modelName": "en-US-DavisNeural",
+        "styleList": [
+            "chat",
+            "angry",
+            "cheerful",
+            "excited",
+            "friendly",
+            "hopeful",
+            "sad",
+            "shouting",
+            "terrified",
+            "unfriendly",
+            "whispering"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Amber",
+        "fullName": "English (United States)",
+        "modelName": "en-US-AmberNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Ana",
+        "fullName": "English (United States)",
+        "modelName": "en-US-AnaNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Ashley",
+        "fullName": "English (United States)",
+        "modelName": "en-US-AshleyNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Brandon",
+        "fullName": "English (United States)",
+        "modelName": "en-US-BrandonNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Christopher",
+        "fullName": "English (United States)",
+        "modelName": "en-US-ChristopherNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Cora",
+        "fullName": "English (United States)",
+        "modelName": "en-US-CoraNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Elizabeth",
+        "fullName": "English (United States)",
+        "modelName": "en-US-ElizabethNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Eric",
+        "fullName": "English (United States)",
+        "modelName": "en-US-EricNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Jacob",
+        "fullName": "English (United States)",
+        "modelName": "en-US-JacobNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Jane",
+        "fullName": "English (United States)",
+        "modelName": "en-US-JaneNeural",
+        "styleList": [
+            "angry",
+            "cheerful",
+            "excited",
+            "friendly",
+            "hopeful",
+            "sad",
+            "shouting",
+            "terrified",
+            "unfriendly",
+            "whispering"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Jason",
+        "fullName": "English (United States)",
+        "modelName": "en-US-JasonNeural",
+        "styleList": [
+            "angry",
+            "cheerful",
+            "excited",
+            "friendly",
+            "hopeful",
+            "sad",
+            "shouting",
+            "terrified",
+            "unfriendly",
+            "whispering"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Michelle",
+        "fullName": "English (United States)",
+        "modelName": "en-US-MichelleNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Monica",
+        "fullName": "English (United States)",
+        "modelName": "en-US-MonicaNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Nancy",
+        "fullName": "English (United States)",
+        "modelName": "en-US-NancyNeural",
+        "styleList": [
+            "angry",
+            "cheerful",
+            "excited",
+            "friendly",
+            "hopeful",
+            "sad",
+            "shouting",
+            "terrified",
+            "unfriendly",
+            "whispering"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Roger",
+        "fullName": "English (United States)",
+        "modelName": "en-US-RogerNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Sara",
+        "fullName": "English (United States)",
+        "modelName": "en-US-SaraNeural",
+        "styleList": [
+            "angry",
+            "cheerful",
+            "excited",
+            "friendly",
+            "hopeful",
+            "sad",
+            "shouting",
+            "terrified",
+            "unfriendly",
+            "whispering"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Steffan",
+        "fullName": "English (United States)",
+        "modelName": "en-US-SteffanNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Tony",
+        "fullName": "English (United States)",
+        "modelName": "en-US-TonyNeural",
+        "styleList": [
+            "angry",
+            "cheerful",
+            "excited",
+            "friendly",
+            "hopeful",
+            "sad",
+            "shouting",
+            "terrified",
+            "unfriendly",
+            "whispering"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "AIGenerate1",
+        "fullName": "English (United States)",
+        "modelName": "en-US-AIGenerate1Neural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "AIGenerate2",
+        "fullName": "English (United States)",
+        "modelName": "en-US-AIGenerate2Neural",
+        "styleList": []
+    },
+    {
+        "gender": 0,
+        "locale": "en-US",
+        "localName": "Blue",
+        "fullName": "English (United States)",
+        "modelName": "en-US-BlueNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "en-US",
+        "localName": "Jenny Multilingual V2",
+        "fullName": "English (United States)",
+        "modelName": "en-US-JennyMultilingualV2Neural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "en-US",
+        "localName": "Ryan Multilingual",
+        "fullName": "English (United States)",
+        "modelName": "en-US-RyanMultilingualNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "ja-JP",
+        "localName": "七海",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-NanamiNeural",
+        "styleList": [
+            "chat",
+            "customerservice",
+            "cheerful"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "ja-JP",
+        "localName": "圭太",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-KeitaNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "ja-JP",
+        "localName": "碧衣",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-AoiNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "ja-JP",
+        "localName": "大智",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-DaichiNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "ja-JP",
+        "localName": "真夕",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-MayuNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "ja-JP",
+        "localName": "直紀",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-NaokiNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "ja-JP",
+        "localName": "志織",
+        "fullName": "Japanese (Japan)",
+        "modelName": "ja-JP-ShioriNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓晓",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoxiaoNeural",
+        "styleList": [
+            "assistant",
+            "chat",
+            "customerservice",
+            "newscast",
+            "affectionate",
+            "angry",
+            "calm",
+            "cheerful",
+            "disgruntled",
+            "fearful",
+            "gentle",
+            "lyrical",
+            "sad",
+            "serious",
+            "poetry-reading",
+            "friendly"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云希",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunxiNeural",
+        "styleList": [
+            "narration-relaxed",
+            "embarrassed",
+            "fearful",
+            "cheerful",
+            "disgruntled",
+            "serious",
+            "angry",
+            "sad",
+            "depressed",
+            "chat",
+            "assistant",
+            "newscast"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云健",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunjianNeural",
+        "styleList": [
+            "narration-relaxed",
+            "sports-commentary",
+            "sports-commentary-excited",
+            "angry",
+            "disgruntled",
+            "cheerful",
+            "sad",
+            "serious",
+            "depressed",
+            "documentary-narration"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓伊",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoyiNeural",
+        "styleList": [
+            "angry",
+            "disgruntled",
+            "affectionate",
+            "cheerful",
+            "fearful",
+            "sad",
+            "embarrassed",
+            "serious",
+            "gentle"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云扬",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunyangNeural",
+        "styleList": [
+            "customerservice",
+            "narration-professional",
+            "newscast-casual"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓辰",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaochenNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓涵",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaohanNeural",
+        "styleList": [
+            "calm",
+            "fearful",
+            "cheerful",
+            "disgruntled",
+            "serious",
+            "angry",
+            "sad",
+            "gentle",
+            "affectionate",
+            "embarrassed"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓梦",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaomengNeural",
+        "styleList": [
+            "chat"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓墨",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaomoNeural",
+        "styleList": [
+            "embarrassed",
+            "calm",
+            "fearful",
+            "cheerful",
+            "disgruntled",
+            "serious",
+            "angry",
+            "sad",
+            "depressed",
+            "affectionate",
+            "gentle",
+            "envious"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓秋",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoqiuNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓睿",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoruiNeural",
+        "styleList": [
+            "calm",
+            "fearful",
+            "angry",
+            "sad"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓双",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoshuangNeural",
+        "styleList": [
+            "chat"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓萱",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoxuanNeural",
+        "styleList": [
+            "calm",
+            "fearful",
+            "cheerful",
+            "disgruntled",
+            "serious",
+            "angry",
+            "gentle",
+            "depressed"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓颜",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoyanNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓悠",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaoyouNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "zh-CN",
+        "localName": "晓甄",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-XiaozhenNeural",
+        "styleList": [
+            "angry",
+            "disgruntled",
+            "cheerful",
+            "fearful",
+            "sad",
+            "serious"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云枫",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunfengNeural",
+        "styleList": [
+            "angry",
+            "disgruntled",
+            "cheerful",
+            "fearful",
+            "sad",
+            "serious",
+            "depressed"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云皓",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunhaoNeural",
+        "styleList": [
+            "advertisement-upbeat"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云夏",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunxiaNeural",
+        "styleList": [
+            "calm",
+            "fearful",
+            "cheerful",
+            "angry",
+            "sad"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云野",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunyeNeural",
+        "styleList": [
+            "embarrassed",
+            "calm",
+            "fearful",
+            "cheerful",
+            "disgruntled",
+            "serious",
+            "angry",
+            "sad"
+        ]
+    },
+    {
+        "gender": 2,
+        "locale": "zh-CN",
+        "localName": "云泽",
+        "fullName": "Chinese (Mandarin, Simplified)",
+        "modelName": "zh-CN-YunzeNeural",
+        "styleList": [
+            "calm",
+            "fearful",
+            "cheerful",
+            "disgruntled",
+            "serious",
+            "angry",
+            "sad",
+            "depressed",
+            "documentary-narration"
+        ]
+    },
+    {
+        "gender": 1,
+        "locale": "zh-HK",
+        "localName": "曉曼",
+        "fullName": "Chinese (Cantonese, Traditional)",
+        "modelName": "zh-HK-HiuMaanNeural",
+        "styleList": []
+    },
+    {
+        "gender": 2,
+        "locale": "zh-HK",
+        "localName": "雲龍",
+        "fullName": "Chinese (Cantonese, Traditional)",
+        "modelName": "zh-HK-WanLungNeural",
+        "styleList": []
+    },
+    {
+        "gender": 1,
+        "locale": "zh-HK",
+        "localName": "曉佳",
+        "fullName": "Chinese (Cantonese, Traditional)",
+        "modelName": "zh-HK-HiuGaaiNeural",
+        "styleList": []
+    }
+]
--- src/views/spoken/AddCharacterCard.vue
+++ src/views/spoken/AddCharacterCard.vue
@@ -8,13 +8,15 @@ import { useSpeechStore } from "@/stores/speechStore";
 import { useSpokenStore } from "@/stores/spokenStore";
 import { useDisplay } from "vuetify";
 import { Gender } from "~/src/enums";
+import initCharacterList from "@/data/characterData";
+import { getLanguageName } from "@/utils/common";
 const speechStore = useSpeechStore();
 const spokenStore = useSpokenStore();
 
 const { mdAndUp } = useDisplay();
 
-const characterList = ref<any>([]);
-const currentCharacter = ref<any>(null);
+const characterList = ref<any>(initCharacterList);
+const currentCharacter = ref<any>("");
 
 const getChracterList = () => {
   const languages = ["ja-JP", "en-US", "zh-CN", "zh-HK"];
@@ -38,6 +40,7 @@ const getChracterList = () => {
 };
 
 onMounted(() => {
+  currentCharacter.value = characterList.value[0];
   getChracterList();
 });
 
@@ -81,7 +84,7 @@ const selectCharacter = (character) => {
 
 const currentStyle = ref("");
 const voiceRate = ref(1);
-const chatTitle = ref("新朋友");
+const chatTitle = ref("");
 const currentStyleList = computed(() => {
   if (currentCharacter.value && currentCharacter.value.styleList.length > 0) {
     currentStyle.value = currentCharacter.value.styleList[0];
@@ -102,7 +105,7 @@ const addChat = () => {
     gender: currentCharacter.value.gender,
     localName: currentCharacter.value.localName,
   };
-  spokenStore.addChat(id, voiceConfig, chatTitle.value);
+  spokenStore.addChat(id, voiceConfig, chatTitle.value?.trim() || "新朋友");
   spokenStore.switchAddCharacterDialog();
 };
 </script>
@@ -116,64 +119,114 @@ const addChat = () => {
   >
     <v-card>
       <v-card-title class="font-weight-bold text-grey-darken-2">
-        角色
+        角色设定
       </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <!-- Rate -->
+        <v-row class="align-center">
+          <v-col cols="12" sm="2" class="pb-sm-3 pb-0">
+            <v-label class="font-weight-bold text-grey-darken-2">姓名</v-label>
+          </v-col>
+          <v-col cols="12" sm="10">
+            <v-text-field
+              v-model="chatTitle"
+              density="compact"
+              variant="outlined"
+              hide-details
+              color="primary"
+              placeholder="口语练习的角色姓名"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" sm="2" class="pb-sm-3 pb-0">
+            <v-label class="font-weight-bold text-grey-darken-2"
+              >语言模型</v-label
+            >
+          </v-col>
+          <v-col cols="12" sm="10">
+            <p class="mb-1 d-flex align-center">
+              <v-chip
+                color="primary"
+                size="small"
+                label
+                class="mr-1"
+                :prepend-icon="
+                  currentCharacter.gender === Gender.Man
+                    ? 'mdi-face-man-shimmer'
+                    : 'mdi-face-woman-shimmer'
+                "
+              >
+                {{ getLanguageName(currentCharacter.locale) }}</v-chip
+              >
+              <span>{{ currentCharacter.localName }}</span>
+            </p>
+            <v-list height="310" class="pa-1" lines="two" density="compact">
+              <!-- ---------------------------------------------- -->
+              <!-- Profile Area -->
+              <!-- ---------------------------------------------- -->
+              <v-list-item
+                @click="selectCharacter(voice)"
+                elevation="1"
+                class="my-1"
+                :active="voice.localName === currentCharacter.localName"
+                v-for="voice in characterList"
+                color="primary"
+              >
+                <template v-slot:prepend>
+                  <v-icon size="30">{{
+                    voice.gender === Gender.Man
+                      ? "mdi-face-man-shimmer"
+                      : "mdi-face-woman-shimmer"
+                  }}</v-icon>
+                </template>
+
+                <v-list-item-title class="font-weight-bold">
+                  {{ voice.localName }}
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ voice.locale }}
+                </v-list-item-subtitle>
+
+                <template v-slot:append>
+                  <span
+                    class="text-body-2 text-grey-lighten-1 font-weight-bold"
+                  >
+                    试听
+                  </span>
+
+                  <v-btn
+                    class="ml-1"
+                    color="grey-lighten-1"
+                    variant="text"
+                    @click.stop="speakTest(voice)"
+                    size="20"
+                  >
+                    <template v-slot:prepend>
+                      <v-icon
+                        size="20"
+                        :color="
+                          voice.localName === currentCharacter.localName
+                            ? 'primary'
+                            : 'primary-lighten-2'
+                        "
+                        >mdi-play-circle-outline</v-icon
+                      >
+                    </template>
+                  </v-btn>
+                </template>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card>
     <v-divider></v-divider>
-    <v-list height="400" class="pa-1" lines="two" density="compact">
-      <!-- ---------------------------------------------- -->
-      <!-- Profile Area -->
-      <!-- ---------------------------------------------- -->
-      <v-list-item
-        @click="selectCharacter(voice)"
-        elevation="1"
-        class="my-1"
-        :active="voice.localName === currentCharacter.localName"
-        v-for="voice in characterList"
-        color="primary"
-      >
-        <template v-slot:prepend>
-          <v-icon size="30">{{
-            voice.gender === Gender.Man
-              ? "mdi-face-man-shimmer"
-              : "mdi-face-woman-shimmer"
-          }}</v-icon>
-        </template>
 
-        <v-list-item-title class="font-weight-bold text-grey-darken-3">
-          {{ voice.localName }}
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          {{ voice.locale }}
-        </v-list-item-subtitle>
+    <v-card> </v-card>
 
-        <template v-slot:append>
-          <span class="text-body-2 text-grey-lighten-1 font-weight-bold">
-            试听
-          </span>
-
-          <v-btn
-            class="ml-1"
-            color="grey-lighten-1"
-            variant="text"
-            @click.stop="speakTest(voice)"
-            size="20"
-          >
-            <template v-slot:prepend>
-              <v-icon
-                size="20"
-                :color="
-                  voice.localName === currentCharacter.localName
-                    ? 'primary'
-                    : 'primary-lighten-2'
-                "
-                >mdi-play-circle-outline</v-icon
-              >
-            </template>
-          </v-btn>
-        </template>
-      </v-list-item>
-    </v-list>
     <v-divider></v-divider>
 
     <v-card min-height="200" class="mt-5">
