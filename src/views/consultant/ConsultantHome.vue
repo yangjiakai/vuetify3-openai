bb63cd1bcaafe811782340d74b19f5e260f22fdd
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-09-02 16:16:04 +0900

    增加3个临时Home页面

--- src/views/consultant/ConsultantHome.vue
+++ src/views/consultant/ConsultantHome.vue
@@ -3,10 +3,23 @@
 * @Maintainer: J.K. Yang
 * @Description: 
 -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Vue3Lottie } from "vue3-lottie";
+</script>
 
 <template>
-  <div class="">consultantHome</div>
+  <div class="d-flex align-center justify-center h-screen bg-white">
+    <div>
+      <Vue3Lottie
+        animationLink="https://lottie.host/03e08e80-d368-4fef-aa7e-f0910d00ea54/aYgluR3DQ2.json"
+        :height="400"
+        :width="400"
+      />
+      <h1 class="text-center font-weight-black text-grey-darken-1">
+        Ask Me AnyThing
+      </h1>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
