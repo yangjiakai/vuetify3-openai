4cc74fb911b64e4a11f301ba9f4d4b378286d35e
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-09-11 01:09:05 +0900

    范文更新成英文

--- src/views/article/ArticlePage.vue
+++ src/views/article/ArticlePage.vue
@@ -17,8 +17,15 @@ const speechStore = useSpeechStore();
 const snackbarStore = useSnackbarStore();
 const articleStore = useArticleStore();
 const sourceArticle = ref(`
-そのせいで大学落ちて浪人することになったのですが、やっと受かった大学にもほとんど行かず、ずっとプログラミングのバイトをしていました。
-夏休みは思いっきりプログラムを書こうと思って、バイト先に入り浸っていたら、正社員よりも給料が高くなってドン引きされたことが記憶に残っています。
+UI stands for user interface. It is the point of contact between humans and computers. Any technology you interact with as a user is part of the user interface. For example, screens, sounds, overall style, and responsiveness are all elements of UI. A user interface involves the following four components:
+
+Navigational elements. Navigational elements help users navigate an interface. Examples of navigational elements in UI include slide bars, search fields, and back arrows. 
+
+Input controls. On-page elements that enable users to input information are input controls. Buttons, checkboxes, and text fields are all examples of input controls. 
+
+Informational components. Informational components are used to communicate information to the user. A progress bar beneath a video or tutorial is an example of an informational component. 
+
+Containers. Containers organize content into easily digestible sections. Rather than listing every subheading underneath a tab, a container element like an accordion menu may be used to hide or show content. 
 `);
 const targetArticle = ref<string[]>([]);
 const transferArtile = (text: string) => {
